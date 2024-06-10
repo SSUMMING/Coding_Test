e1a4b337f6bcdd3c479f93511d2ad7cbe9b1e493
SSUMMING <imgodssumming@gmail.com>
2024-06-11 02:32:20 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 11.21 ms, Memory: 7.5 MB -BaekjoonHub

--- 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.py
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.py
@@ -1,8 +1,4 @@
-# str, n = input().strip().split(' ')
-# n = int(n)
-
-input_0 =input().strip().split(' ')
-str = input_0[0]
-n = int(input_0[1])
+str, n = input().strip().split(' ')
+n = int(n)
 
 print(str*n)

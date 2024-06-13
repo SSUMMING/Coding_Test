eb04144fdabaa64a03de08b59c387790ad2780b1
SSUMMING <imgodssumming@gmail.com>
2024-06-14 02:02:12 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,12 +1,4 @@
 def solution(a, b):
     answer = 0
-    if str(a)+str(b) > str(b)+str(a):
-        answer += int(str(a)+str(b))
-    elif str(a)+str(b) < str(b)+str(a):
-        answer += int(str(b)+str(a))
-    else:
-        answer += int(str(a)+str(b))
-    
+    answer += max(int(str(a)+str(b)), int(str(b)+str(a)))
     return answer
-
-   

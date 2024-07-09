d8b6990d5c01913f0ac8d0e44a4c6dbcbacfd404
SSUMMING <imgodssumming@gmail.com>
2024-07-09 21:30:46 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,9 @@
+# 숫자 input 리스트로 만들기 
+N = list(map(int,input().split()))
+
+if N == sorted(N):
+    print('ascending')
+elif N == sorted(N,reverse=True):
+    print('descending')
+else:
+    print('mixed')

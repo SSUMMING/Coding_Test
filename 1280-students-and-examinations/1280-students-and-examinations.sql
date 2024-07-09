252d361e90e2067f66efb6d13a5aac110622676f
SSUMMING <imgodssumming@gmail.com>
2024-07-09 22:49:56 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,6 @@
+SELECT S.student_id ,S.student_name ,Sub.subject_name, COUNT(S.student_id) AS attended_exams 
+FROM Students S
+CROSS JOIN Subjects Sub
+LEFT JOIN Examinations E ON S.student_id = E.student_id AND E.subject_name = Sub.subject_name
+GROUP BY S.student_id ,student_name ,Sub.subject_name
+ORDER BY S.student_id, Sub.subject_name

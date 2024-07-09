ef43c68e425303327af9b8af030b72e61b081e90
SSUMMING <imgodssumming@gmail.com>
2024-07-09 22:53:04 +0900

    Time: 1103 ms (47.94%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,4 +1,4 @@
-SELECT S.student_id ,S.student_name ,Sub.subject_name, COUNT(S.student_id) AS attended_exams 
+SELECT S.student_id ,S.student_name ,Sub.subject_name, COUNT(E.student_id) AS attended_exams 
 FROM Students S
 CROSS JOIN Subjects Sub
 LEFT JOIN Examinations E ON S.student_id = E.student_id AND E.subject_name = Sub.subject_name

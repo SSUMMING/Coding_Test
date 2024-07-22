e371fd049de1c9b1858edd1e98b4168777a1c21a
SSUMMING <imgodssumming@gmail.com>
2024-07-22 20:51:58 +0900

    Time: 669 ms (62.7%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -3,5 +3,5 @@ SELECT MAX(product_name) AS product_name, SUM(unit) AS unit
 FROM Products P
 JOIN Orders O ON P.product_id = O.product_id
 WHERE DATE_FORMAT(order_date, '%Y-%m') = '2020-02'
-GROUP BY O.product_id, product_name, order_date
+GROUP BY O.product_id, product_name
 HAVING SUM(unit) >= 100

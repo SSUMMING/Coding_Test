850cee59582819748bb0ea89ee6be43e295d22ea
SSUMMING <imgodssumming@gmail.com>
2024-07-22 20:34:22 +0900

    Time: 407 ms (77.89%), Space: 0B (100%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -1,5 +1,5 @@
 -- 날짜별로 판매된 다양한 제품의 수와 이름을 찾는 솔루션을 작성하세요.
-SELECT sell_date, COUNT(DISTINCT product) AS num_sold, GROUP_CONCAT(product ORDER BY product) AS products
+SELECT sell_date, COUNT(DISTINCT product) AS num_sold, GROUP_CONCAT(DISTINCT product ORDER BY product) AS products
 FROM Activities
 GROUP BY sell_date
 ORDER BY 1

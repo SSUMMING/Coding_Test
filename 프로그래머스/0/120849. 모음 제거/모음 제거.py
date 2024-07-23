d139a2cb1eb44d53aa199dc79a51a31f10053a46
SSUMMING <imgodssumming@gmail.com>
2024-07-23 21:07:09 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,7 +1,8 @@
 def solution(my_string):
     answer = ''
-    odd = ['a','e','i','o','u']
-    for e in my_string:
-        if e not in odd:
-            answer += e
+    for item in my_string:
+        if item == 'a' or item == 'e' or item == 'i' or item == 'o' or item == 'u':
+            answer
+        else:
+            answer += item 
     return answer

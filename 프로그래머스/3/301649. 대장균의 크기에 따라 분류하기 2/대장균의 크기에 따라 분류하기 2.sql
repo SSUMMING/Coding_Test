7528b649bfc59b8c155ca9f1b8911ef99a03cf97
SSUMMING <imgodssumming@gmail.com>
2024-08-06 20:15:08 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,12 +1,12 @@
-WITH size_ranking AS(
-SELECT *, ROW_NUMBER() OVER(ORDER BY SIZE_OF_COLONY DESC) AS row_desc
-FROM ECOLI_DATA)
-
 SELECT ID, 
-        CASE WHEN row_desc <= ((SELECT COUNT(*) FROM size_ranking)*0.25 ) THEN 'CRITICAL'
-             WHEN row_desc <= ((SELECT COUNT(*) FROM size_ranking)*0.5 ) THEN 'HIGH'
-             WHEN row_desc <= ((SELECT COUNT(*) FROM size_ranking)*0.75 ) THEN 'MEDIUM'
-             WHEN row_desc <= (SELECT COUNT(*) FROM size_ranking) THEN 'LOW' 
-             END AS COLONY_NAME
-FROM size_ranking
-ORDER BY ID
+    CASE WHEN rank_size <= ((SELECT COUNT(*) FROM ECOLI_DATA) * 0.25) THEN 'CRITICAL'
+         WHEN rank_size <= ((SELECT COUNT(*) FROM ECOLI_DATA) * 0.50) THEN 'HIGH'
+         WHEN rank_size <= ((SELECT COUNT(*) FROM ECOLI_DATA) * 0.75) THEN 'MEDIUM'
+         ELSE 'LOW'
+    END AS COLONY_NAME
+FROM
+(
+    SELECT ID, RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS rank_size
+    FROM ECOLI_DATA
+) AS aa
+ORDER BY 1;

e47dfe5bacbf36352642565e1e6990163bf6dc9e
SSUMMING <imgodssumming@gmail.com>
2024-09-04 23:51:49 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,22 +1,16 @@
-# 사번, 성명, 평가등급(분기별 점수 평균), 보너스(연봉*20%)
-SELECT EMP_NO, EMP_NAME, GRADE,
-        CASE
-        WHEN GRADE = 'S' THEN SAL*0.2
-        WHEN GRADE = 'A' THEN SAL * 0.15
-        WHEN GRADE = 'B' THEN SAL * 0.1
-        ELSE SAL * 0
-        END AS BONUS
-FROM(SELECT G.EMP_NO, EMP_NAME, SAL, AVG(SCORE),
-        CASE 
-        WHEN AVG(SCORE) >= 96  THEN 'S'
-        WHEN AVG(SCORE) >= 90 THEN 'A'
-        WHEN AVG(SCORE) >= 80 THEN 'B'
-        ELSE 'C'
-        END AS GRADE
-FROM HR_EMPLOYEES E
-JOIN HR_GRADE G ON E.EMP_NO = G.EMP_NO
-GROUP BY G.EMP_NO) AS A
-ORDER BY 1
+with GRADE as (
+            select EMP_NO,
+             (case when avg(SCORE) >= 96 then 'S'
+                        when avg(SCORE) >= 90 then 'A'
+                        when avg(SCORE) >= 80 then 'B'
+                        else 'C' end ) as GRADE,
+            (case when avg(SCORE) >= 96 then 0.2
+                                when avg(SCORE) >= 90 then 0.15
+                                when avg(SCORE) >= 80 then 0.1
+                                else 0 end ) as BONUS
+            from HR_GRADE
+            group by EMP_NO)
 
-
-# 사원1 
+select e.EMP_NO, e.EMP_NAME, g.GRADE, g.BONUS * e.SAL as BONUS
+from HR_EMPLOYEES e join GRADE g on e.EMP_NO = g.EMP_NO 
+order by e.EMP_NO asc;

86af64e318a0713f58a6d0fa5a02fc2f4fefec67
SSUMMING <imgodssumming@gmail.com>
2024-09-26 16:59:27 +0900

    Time: 541 ms (91.21%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,19 +1,5 @@
-WITH total AS(
-SELECT S.user_id, 
-       CASE WHEN action IS NULL THEN 0
-            ELSE COUNT(*) 
-            END AS total_request
-FROM Signups S 
+SELECT S.user_id,
+       ROUND(COUNT(CASE WHEN action = 'confirmed' THEN S.user_id END) / COUNT(*) ,2) AS confirmation_rate
+FROM Signups S
 LEFT JOIN Confirmations C ON S.user_id = C.user_id
 GROUP BY S.user_id
-) 
-,
-confirmed AS(
-SELECT user_id, COUNT(*) AS confirmed_request
-FROM Confirmations 
-WHERE action = 'confirmed'
-GROUP BY user_id)
-
-SELECT T.user_id, 
-        IFNULL(ROUND(confirmed_request/total_request,2), 0) AS confirmation_rate
-FROM total T LEFT JOIN confirmed  CF ON T.user_id = CF.user_id

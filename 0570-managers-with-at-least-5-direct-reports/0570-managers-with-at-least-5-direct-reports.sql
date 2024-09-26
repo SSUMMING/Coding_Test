0bb2f18ba5c1ecf01f1a9c21306baba4479d1e1a
SSUMMING <imgodssumming@gmail.com>
2024-09-26 16:47:41 +0900

    Time: 298 ms (91.4%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,6 @@
-SELECT NAME
+SELECT name
 FROM Employee
-WHERE id IN (SELECT managerId 
-            FROM Employee
-            WHERE managerId  IS NOT NULL
-            GROUP BY managerId 
-            HAVING COUNT(*) >= 5 )
+WHERE id IN (SELECT managerId
+FROM Employee
+GROUP BY managerId
+HAVING COUNT(*) >= 5);

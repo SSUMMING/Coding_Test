a8f0f56b214ab24a8c22d9d021b3028c420c1a3c
SSUMMING <imgodssumming@gmail.com>
2024-10-01 21:20:21 +0900

    Time: 1161 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,24 +1,8 @@
-# start 테이블 (SUM) : 기계별 star 시간의 합 
-WITH start AS(
-SELECT machine_id, process_id , timestamp 
-FROM Activity
-WHERE activity_type = 'start'
-GROUP BY machine_id,process_id
-),
-end AS(
-# end 테이블 : 기계별 end 시간의 합
-SELECT machine_id, process_id,  timestamp 
-FROM Activity
-WHERE activity_type = 'end'
-GROUP BY machine_id,process_id
-),
-base AS(
-SELECT machine_id, process_id
-FROM Activity
-GROUP BY machine_id, process_id)
-
-SELECT B.machine_id, ROUND(AVG(E.timestamp- S.timestamp),3) AS processing_time 
-FROM base B
-LEFT JOIN start S ON B.machine_id = S.machine_id AND B.process_id = S.process_id
-LEFT JOIN end E ON E.machine_id = S.machine_id AND E.process_id = S.process_id
-GROUP BY machine_id
+WITH process AS(
+SELECT A.machine_id,  A.process_id, (A1.timestamp - A.timestamp) AS process
+FROM Activity A 
+JOIN Activity A1 ON A.machine_id = A1.machine_id AND A.process_id = A1.process_id AND A.timestamp < A1.timestamp
+)
+SELECT machine_id, ROUND(AVG(process),3) AS processing_time
+FROM process
+GROUp BY machine_id
